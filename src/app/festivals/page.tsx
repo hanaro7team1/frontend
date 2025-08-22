c694a9794b34ebc770c26ca8660c38585b38f45f
ryd10227 <ryd10227@gmail.com>
2025-08-22 15:37:45 +0900

    Feat: 기본 페이지 컴포넌트 생성

---
+++ src/app/festivals/page.tsx
@@ -0,0 +1,10 @@
+import { BottomTabNav, Header } from '@/components/organisms';
+
+export default function FestivalsPage() {
+  return (
+    <div>
+      <Header title='지역 축제' />
+      <BottomTabNav />
+    </div>
+  );
+}
---
+++ src/app/realEstates/page.tsx
@@ -0,0 +1,10 @@
+import { BottomTabNav, Header } from '@/components/organisms';
+
+export default function RealEstatesPage() {
+  return (
+    <div>
+      <Header title='매물 찾기' />
+      <BottomTabNav />
+    </div>
+  );
+}
