30327b1829b3af414d1a6bc51dcdb4d2490ca02f
ryd10227 <ryd10227@gmail.com>
2025-08-22 10:58:33 +0900

    Refactor: atom 컴포넌트 (그림자 있는 기본 레이아웃) 기타 props 받을 수 있도록 수정

--- src/components/atoms/ShadowBox.tsx
+++ src/components/atoms/ShadowBox.tsx
@@ -3,14 +3,21 @@ import { cn } from '@/lib/utils';
 
 type Props = {
   onClick?: () => void;
+  className?: string;
 };
 
 /**
  * 그림자 테두리가 있는 레이아웃 컴포넌트
  * @param onClick - 클릭 이벤트 핸들러
+ * @param className - 추가 스타일 클래스
  * @children - 레이아웃 안에 렌더링할 자식 요소
  */
-export default function ShadowBox({ onClick, children }: PropsWithChildren<Props>) {
+export default function ShadowBox({
+  onClick,
+  className,
+  children,
+  ...props
+}: PropsWithChildren<Props>) {
   return (
     <div
       onClick={onClick}
@@ -19,7 +26,9 @@ export default function ShadowBox({ onClick, children }: PropsWithChildren<Props
         {
           'cursor-pointer': onClick,
         },
+        className,
       )}
+      {...props}
     >
       {children}
     </div>
