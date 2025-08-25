4b030344120ac86c9c8326e1e0eb2aa4925f5c06
ryd10227 <ryd10227@gmail.com>
2025-08-25 17:16:59 +0900

    Feat: 사랑방 목록 페이지 (숙박 탭)

--- src/app/stays/page.tsx
+++ src/app/stays/page.tsx
@@ -1,10 +1,30 @@
 import { BottomTabNav, Header } from '@/components/common';
+import { RoomList, RoomTypeFilter, StayOptionFilters } from '@/components/domain/stays';
+import { RoomType } from '@/types/stays';
 
-export default function StaysPage() {
+type Props = {
+  searchParams: Promise<{
+    roomType?: RoomType;
+    location?: string;
+    schedule?: string;
+    peopleCount?: string;
+  }>;
+};
+
+export default async function StaysPage({ searchParams }: Props) {
+  const { roomType, location, schedule, peopleCount } = await searchParams;
+
+  const isAdmin = false; // TODO: 추후에 세션에서 관리자 여부 읽어오기
+
   return (
     <div>
-      <Header title='사랑방 찾기' />
-      <BottomTabNav />
+      <Header title='사랑방 찾기' bgColor='green' />
+      <main className='relative'>
+        <RoomTypeFilter roomType={roomType} />
+        <StayOptionFilters stayOption={{ location, schedule, peopleCount }} />
+        <RoomList />
+      </main>
+      {!isAdmin && <BottomTabNav />}
     </div>
   );
 }
