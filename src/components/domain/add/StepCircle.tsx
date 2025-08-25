d4df3c7b5cd2dcefa459445ed03a13bfab5d6606
ahhyun <ahhyun1106@gmail.com>
2025-08-25 11:36:55 +0900

    Design: StepProgress 색상 green에서 pink로 변경

---
+++ src/components/domain/add/StepCircle.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils';
+import { Txt } from '@/components/atoms';
+import { STEP_TEXT_SIZE } from '@/constants/common/StepCircle';
+
+type Props = {
+  stepNumber: number;
+  stepName: string;
+  isActive?: boolean;
+};
+
+export default function StepCircle({ stepNumber, stepName, isActive }: Props) {
+  return (
+    <div className='relative z-10 flex flex-col items-center px-1'>
+      <div
+        className={cn(
+          'bg-gray-6d6 inline-flex h-10 w-10 items-center justify-center rounded-full',
+          { 'bg-pink-a76': isActive },
+        )}
+      >
+        <Txt className='text-white'>{stepNumber}</Txt>
+      </div>
+      <div className='text-center leading-tight'>
+        <Txt
+          size={STEP_TEXT_SIZE}
+          className={cn('text-gray-6d6', {
+            'text-pink-a76': isActive,
+          })}
+        >
+          {stepName}
+        </Txt>
+      </div>
+    </div>
+  );
+}
