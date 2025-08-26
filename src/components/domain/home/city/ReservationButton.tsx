71357ae1af4d3d79d048fe9a37674c73e0d5c1b2
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-26 15:34:07 +0900

    Design: UI 세부 수정

--- src/components/domain/home/city/ReservationButton.tsx
+++ src/components/domain/home/city/ReservationButton.tsx
@@ -10,11 +10,11 @@ export default function ReservationButton() {
   return (
     <button
       className={
-        'bg-green-49d flex w-full items-center gap-2 rounded-[10px] py-[21px] pl-5'
+        'bg-green-49d flex w-full items-center gap-3 rounded-[10px] py-[21px] pl-5'
       }
       onClick={() => router.push('/reservations')}
     >
-      <List className='items-center text-white' />
+      <List className='items-center text-white' size={32} />
       <Txt className='text-white' size={22} weight='bold'>
         나의 예약 목록 보기
       </Txt>
