daf89648ef91e23475ff570fdbdae992d22a8fd6
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-26 15:55:04 +0900

    Fix: 도시 시니어 이름 부분 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,12 +13,13 @@ export default async function HomePage({ params }: Props) {
 
   const reservation = {
     id: reservationId,
+    user: '윤서연',
     name: '가람마을 사랑방 4호',
     dateRange: '25.09.09 (화) - 25.09.13 (토)',
     imgUrl: '/images/sample2.png',
   };
 
-  const { name, dateRange, imgUrl } = reservation;
+  const { user, name, dateRange, imgUrl } = reservation;
 
   return (
     <div className='relative flex flex-col'>
@@ -26,7 +27,7 @@ export default async function HomePage({ params }: Props) {
       <header className='z-1 flex items-center justify-center'>
         <Image src='/images/Img_SIDO_LOGO.png' alt='시도 로고' width={86} height={86} />
         <Txt size={22} weight='medium' align='left'>
-          하는 윤서연 님, 환영합니다
+          하는 {user} 님, 환영합니다
         </Txt>
       </header>
 
