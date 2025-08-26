d1a62c07f7040814bff204678e0e9499f3671ecd
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-26 17:58:58 +0900

    Feat: 시골 관리자 홈 페이지 구현

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,51 @@
-export default function AdminPage() {
-  return <div>시골 관리자 홈</div>;
+import Image from 'next/image';
+import { Txt } from '@/components/atoms';
+import { Bankbook, MenuTabs, ReservationStats } from '@/components/domain/admin';
+
+export default function HomePage() {
+  // TODO: 실제 API 연동 → getVillageAccount(), getReservationStats()
+  const user = {
+    name: '가람마을',
+  };
+
+  const account = {
+    accountName: '시도통장',
+    accountNumber: '1234-56789-0000',
+    balance: '3,000,000원',
+  };
+
+  const reservationStats = {
+    booked: 12,
+    staying: 2,
+    completed: 32,
+  };
+
+  const { name } = user;
+  const { accountName, accountNumber, balance } = account;
+  const { booked, staying, completed } = reservationStats;
+
+  return (
+    <div className='relative flex flex-col'>
+      <div className='from-pink-a76/13 absolute top-0 z-0 h-49 w-full bg-gradient-to-b to-white' />
+
+      <header className='z-1 flex items-center justify-center'>
+        <Txt size={22} weight='medium'>
+          {name}의
+        </Txt>
+        <Image src='/images/Img_SIDO_LOGO.png' alt='시도 로고' width={86} height={86} />
+        <Txt size={22} weight='medium'>
+          를 응원합니다
+        </Txt>
+      </header>
+
+      <main className='z-1 space-y-5 px-4'>
+        <Bankbook accountName={accountName} accountNumber={accountNumber} balance={balance} />
+        <ReservationStats booked={booked} staying={staying} completed={completed} />
+      </main>
+
+      <footer className='mt-9 px-4'>
+        <MenuTabs />
+      </footer>
+    </div>
+  );
 }
