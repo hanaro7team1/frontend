fa0889fc5bbee106602f9509f414f5142cc018e5
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-26 10:26:08 +0900

    Feat: 예약 상태 캡슐 추가

--- src/components/domain/stays/StayHeader.tsx
+++ src/components/domain/stays/StayHeader.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Txt from '@/components/atoms/Text';
+import StatusCapsule from './StatusCapsule';
 
 type Props = {
   title: string;
@@ -9,11 +10,17 @@ type Props = {
 export default function StayHeader({ title, address }: Props) {
   return (
     <div className='flex flex-col gap-2'>
-      <div className='flex items-center gap-2'>
-        <Image src='/icons/Ic_House_big.svg' alt='home-icon' width={28} height={27} />
-        <Txt size={25} weight='bold' align='left'>
-          {title}
-        </Txt>
+      <div className='flex items-center justify-between'>
+        <div className='flex gap-2'>
+          <Image src='/icons/Ic_House_big.svg' alt='home-icon' width={28} height={27} />
+          <Txt size={25} weight='bold' align='left'>
+            {title}
+          </Txt>
+        </div>
+        <div className='justify-end'>
+          {/* TODO: 실제 예약 상태 연동 */}
+          <StatusCapsule status={'예약 가능'} />
+        </div>
       </div>
 
       <Txt align='left'>{address}</Txt>
