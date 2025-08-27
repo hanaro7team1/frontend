6fb565ab52a729b7bb01b00fc3054227926b2638
ryd10227 <ryd10227@gmail.com>
2025-08-28 05:43:03 +0900

    Feat: 사랑방 정보 수정 페이지

--- src/app/admin/stays/[id]/page.tsx
+++ src/app/admin/stays/[id]/page.tsx
@@ -1,3 +1,13 @@
+import { Txt } from '@/components/atoms';
+import { Header } from '@/components/common';
+import {
+  EditStayArea,
+  EditStayCapacity,
+  EditStayDescription,
+} from '@/components/domain/admin/edit';
+import { StayInfoCard } from '@/components/domain/reservations';
+import { stayDetail } from '../../../../../public/dummy';
+
 type Props = {
   params: Promise<{ id: string }>;
 };
@@ -5,5 +15,23 @@ type Props = {
 export default async function AdminStayEditPage({ params }: Props) {
   const { id } = await params;
 
-  return <div>{id}번 사랑방 정보 수정</div>;
+  const { stayPicURL, stayId, stayName, stayAddress, capacity, area, description } = stayDetail;
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <Header title='사랑방 정보 수정하기' />
+
+      <main className='flex flex-col gap-10 px-5'>
+        <StayInfoCard data={{ stayPicURL, stayId, stayName, stayAddress }} isAdmin />
+        <div className='flex flex-col gap-4'>
+          <Txt size={22}>사랑방 정보 수정</Txt>
+          <div className='flex flex-col gap-3'>
+            <EditStayArea area={area} />
+            <EditStayCapacity capacity={capacity} />
+          </div>
+        </div>
+        <EditStayDescription data={{ description }} />
+      </main>
+    </div>
+  );
 }
