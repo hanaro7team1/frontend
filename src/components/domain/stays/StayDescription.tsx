97ed9208ffdb7d40e428d32167a4c82164518a16
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-27 09:53:53 +0900

    Refactor: Txt 컴포넌트 align=left 속성 제거

--- src/components/domain/stays/StayDescription.tsx
+++ src/components/domain/stays/StayDescription.tsx
@@ -9,7 +9,7 @@ export default function StayDescription({ item, mode }: Props) {
   return (
     <div className={mode === 'city' ? 'h-45 w-full overflow-y-auto' : 'h-72 w-full'}>
       <div className='flex flex-col'>
-        <Txt size={18} align='left' className='text-gray-070'>
+        <Txt size={18} className='text-gray-070'>
           {item}
         </Txt>
       </div>
