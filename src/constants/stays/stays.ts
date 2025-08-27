07bc6910ce46682712c5f41595a73d4bbba8ab6e
ryd10227 <ryd10227@gmail.com>
2025-08-27 15:18:29 +0900

    Fix: 하숙형/독립형 선택 시 기존 searchParam 없어지는 현상 해결

---
+++ src/constants/stays/stays.ts
@@ -0,0 +1,2 @@
+export const STAYS_URL = '/stays';
+export const ROOM_TYPES = ['하숙형', '독립형'];
--- src/types/stays.ts
+++ src/types/stays.ts
@@ -1,2 +1,19 @@
+import { ReactElement } from 'react';
+
+export type StaysSearchParams = {
+  roomType?: RoomType;
+  location?: string;
+  schedule?: string;
+  peopleCount?: string;
+};
+
 export type RoomType = '하숙형' | '독립형';
 export type RoomStatus = '예약 가능' | '예약 마감' | '예약 닫힘';
+
+export type StayOptionFilter = {
+  key: string;
+  label: string;
+  valueSize?: number;
+  defaultValue: string;
+  BottomSheetType: ReactElement;
+};
