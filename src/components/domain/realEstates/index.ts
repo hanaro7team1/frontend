07c5a0f06ba94673a0c2ef95863992a8fa28f44d
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-27 15:13:26 +0900

    Feat: 매물 상세 페이지 UI 구현

---
+++ src/components/domain/realEstates/index.ts
@@ -0,0 +1,3 @@
+export { default as EstateDescription } from './EstateDescription';
+export { default as BottomButton } from './BottomButton';
+export { default as EstateHeader } from './EstateHeader';
