65a9c88f2f8837cd2ae1254e256e87dbbf8fe53b
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-28 14:13:34 +0900

    Fix: 페이지 이동 시 필터 값이 누락되는 버그 수정

--- src/components/domain/stays/RoomList.tsx
+++ src/components/domain/stays/RoomList.tsx
@@ -1,15 +1,17 @@
+import { StaysSearchParams } from '@/types/stays';
 import { RoomItem } from '.';
 import { dummyRooms } from '../../../../public/dummy';
 
 type Props = {
   isAdmin?: boolean;
+  searchParams: StaysSearchParams;
 };
 
-export default function RoomList({ isAdmin = false }: Props) {
+export default function RoomList({ isAdmin = false, searchParams }: Props) {
   return (
     <div className='m-4 space-y-4'>
       {dummyRooms.map((room) => (
-        <RoomItem key={room.id} data={room} isAdmin={isAdmin} />
+        <RoomItem key={room.id} data={room} isAdmin={isAdmin} searchParams={searchParams} />
       ))}
     </div>
   );
