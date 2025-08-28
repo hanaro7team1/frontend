df333655d9cc631524de22e335eb1193f2e9eff6
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-08-28 13:34:35 +0900

    Fix: 기존 연도 4자리를 2자리 보이게 수정

--- src/utils/stays/stays.ts
+++ src/utils/stays/stays.ts
@@ -1,10 +1,20 @@
 /**
- * Date객체를 2025.08.27 형태로 format해주는 함수
+ * Date객체를 YY.MM.DD 형태로 format해주는 함수
  * @param date : Date 객체
- * @returns 2025.08.27 형태의 string
+ * @returns YY.MM.DD 형태의 string
  */
-export const formatDate = (date?: Date): string =>
-  date ? date.toISOString().split('T')[0].replaceAll('-', '.') : '';
+export const formatDate = (date?: Date): string => {
+  if (!date) return '';
+
+  // getFullYear()로 4자리 연도를 가져와서 .slice(-2)로 2자리로 변경.
+  const year = date.getFullYear().toString().slice(-2);
+
+  // getMonth()는 0부터 시작하므로 (0 = 1월) 1을 더하기
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
+};
 
 /**
  * 오늘, 모레 Date객체를 배열에 담아 return하는 함수
