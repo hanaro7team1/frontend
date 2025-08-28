cd570555cdb072ac12f5b587acfb780e2e288910
Kunbam7 <kunh9905@gmail.com>
2025-08-28 13:16:54 +0900

    edit signin page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,3 +1,22 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import type React from 'react';
+import { FormEvent, useState } from 'react';
+import { PasswordField } from '@/components/domain/admin/auth/PasswordField';
+
 export default function SignInPage() {
-  return <div>로그인</div>;
+  
+
+  return (
+    <div>
+      {/* <Image src='/images/Img_SIDO_LOGO.png' alt='시도 로고' width={86} height={86} /> */}
+      로그인
+
+      {/* <PasswordField placeholder='비밀번호를 입력해 주세요' /> */}
+      
+    </div>
+
+  );
 }
