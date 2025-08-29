53c6fe865cb22b4c8a7f82a6f0c381e810a85974
Kunbam7 <kunh9905@gmail.com>
2025-08-29 16:19:19 +0900

    feat: listbox component

--- src/app/festivals/page.tsx
+++ src/app/festivals/page.tsx
@@ -1,17 +1,16 @@
 import { BottomTabNav, Header } from '@/components/common';
 import ListBox from '@/components/domain/festivals/ListBox';
 import Image from 'next/image';
-import { title } from 'process';
-import { ur } from 'zod/v4/locales';
 
 export default function FestivalsPage() {
+  // db
   const smapleFestival = {
     img:"/images/dummy_image.png",
     title: "신정호 별빛축제",
     startDate:"2025.09.01",
     endDate:"2025.09.10",
     city:"충청남도 아산시",
-    url: "/admin/mypage"
+    url:"https://github.com/hanaro7team1/frontend"
   }
   const {img, title, startDate, endDate, city, url} = smapleFestival;
 
@@ -20,7 +19,7 @@ export default function FestivalsPage() {
   return <>
     <Header title='지역 축제' bgColor='green'/>
       
-    <div className='flex flex-col p-4'>
+    <div className='flex p-4'>
       <Image src="/images/Img_Festival.svg" alt='쇼핑' width={100} height={100} />
 
     </div>
--- src/components/domain/festivals/ListBox.tsx
+++ src/components/domain/festivals/ListBox.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Txt } from "@/components/atoms";
+import { Button, Txt } from "@/components/atoms";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -18,12 +18,12 @@ export default function ListBox({img, title, startDate, endDate, city, url}:Prop
 
     return <>
         <div className="flex gap-2">
-            <Image src={img} alt="축제" width={154} height={154} className="rounded-[8px]"/>
+            <Image src={img} alt="축제" width={150} height={150} className="rounded-[8px] shrink-0"/>
             <div className="flex flex-col">
                 <Txt size={20}>{title}</Txt>
                 <Txt size={18}>{startDate} ~ {endDate}</Txt>
                 <Txt size={15}>{city}</Txt>
-                <button title="상세보기" className="w-[195px] h-[35px] "onClick={() => router.push(`${url}`)}/>
+                <Button title="상세보기" className="h-[35px] bg-gray-6d6 text-black" onClick={() => router.push(`${url}`)}/>
             </div>
         </div>
     </>;
