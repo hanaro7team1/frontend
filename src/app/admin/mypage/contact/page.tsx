05c1bb30a2b6bc0f608550d2ac2ce0263dadc7ef
Kunbam7 <kunh9905@gmail.com>
2025-08-29 14:41:58 +0900

    Feat: mypage pages & components

--- src/app/admin/mypage/contact/page.tsx
+++ src/app/admin/mypage/contact/page.tsx
@@ -1,3 +1,45 @@
+'use client'
+import { Button, Input, Txt } from "@/components/atoms";
+import { Header } from "@/components/common";
+import { useState } from "react";
+import Image from "next/image";
+import NoticeModal from "@/components/domain/admin/mypage/NoticeModal";
+
 export default function AdminContactPage() {
-  return <div>시골 시니어 마이페이지 - 연락처 변경</div>;
+  // db
+  const hostPhone = "010-1234-1234";
+
+  const [phone, setPhone] = useState(hostPhone);
+  const [newPhone, setNewPhone] = useState("");
+  const [openNotice, setOpenNotice] = useState(false);
+
+  const editPhone = () => {
+    setPhone(newPhone.trim());
+    setOpenNotice(true);
+  };
+
+  return (
+    <>
+      <Header title="전화번호 변경"/>
+      
+      <div className="p-8 flex flex-col gap-4">
+        <Txt size={24} className="text-gray-070">기존 전화번호</Txt>
+        <div className="flex items-center gap-3">
+          <Image src='/icons/Ic_Phone_big.svg' alt="수화기" width={35} height={35}/>
+          <Txt size={24}>{phone}</Txt>
+        </div>
+      
+        <div className="flex flex-col gap-4 mt-[30px]">
+          <Txt size={24} className="text-gray-070">변경할 전화번호</Txt>
+          <Input value={newPhone} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewPhone(e.target.value)}
+            placeholder="변경할 전화번호를 입력하세요"/>
+        </div>
+        
+        <Button title="변경하기" color="pink" onClick={editPhone}/>
+      </div>
+
+      <NoticeModal open={openNotice} text="전화번호" />
+    </>
+  );
 }
+
---
+++ src/components/domain/admin/mypage/NoticeModal.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Button, Txt } from "@/components/atoms";
+import { useRouter } from "next/navigation";
+
+type Props = {
+    open: boolean;
+    text?: string;
+};
+
+export default function NoticeModal({open, text}: Props) {
+    const router = useRouter();
+    if(!open){ return null;}
+    
+    const message = `${text}가 변경되었어요`;
+
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4">
+            <div className="w-[372px] h-[193px] shrink-0 rounded-[10px]
+                    border border-[#DEDEDE] bg-white p-5 shadow-md
+                    flex flex-col items-center justify-between">
+                <Txt size={26} className="text-center">{message}</Txt>
+                <Button title="확인" color="pink" className="w-[337px] h-[56px]"
+                    onClick={() => router.back()}/>
+            </div>
+        </div>
+    );
+}
