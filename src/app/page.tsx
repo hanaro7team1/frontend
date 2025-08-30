2b7005613ff3c060bd5266dec4f7827068deb77e
ryd10227 <ryd10227@gmail.com>
2025-08-31 00:00:15 +0900

    Feat: 스플래시 화면 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import { Txt } from '@/components/atoms';
+
 export default function SplashPage() {
-  return <div>스플래시</div>;
+  return (
+    <div className='bg-gray-7f9 flex h-screen flex-col items-center justify-center gap-5'>
+      <div className='flex flex-col items-center'>
+        <Image src='/images/Img_SIDO_LOGO_2.png' alt='시도 로고' width={100} height={100} />
+        <Txt className='text-gray-070'>시골과 도시의 연결</Txt>
+      </div>
+
+      <Image src='/images/Img_Splash.svg' alt='시도 로고' width={200} height={230} />
+    </div>
+  );
 }
