6fe9fb46c384d9f06f0793aa241adb9a9065f91b
ryd10227 <ryd10227@gmail.com>
2025-08-30 23:16:16 +0900

    Feat: 매물 목록 화면 필터 컴포넌트 (지역, 거래형태, 가격)

---
+++ src/components/domain/realEstates/FilterEstatesOptions.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { Txt } from '@/components/atoms';
+import { OptionFilter } from '@/types/stays';
+import { BottomSheetDealType, BottomSheetPriceRange } from '.';
+import { BottomSheetLocation } from '../stays';
+
+export default function FilterEstatesOptions() {
+  const searchParams = useSearchParams();
+  const location = searchParams.get('location');
+  const dealType = searchParams.get('dealType');
+  const priceRange = searchParams.get('priceRange');
+
+  const FILTERS: OptionFilter[] = [
+    {
+      key: 'region',
+      label: '지역',
+      valueSize: 22,
+      defaultValue: location?.split(' ')[1] || '전체',
+      BottomSheetType: <BottomSheetLocation />,
+    },
+    {
+      key: 'dealType',
+      label: '거래형태',
+      valueSize: 22,
+      defaultValue: dealType || '전세',
+      BottomSheetType: <BottomSheetDealType />,
+    },
+    {
+      key: 'priceRange',
+      label: '가격',
+      valueSize: 16,
+      defaultValue: priceRange || '4000만원\n-\n6000만원',
+      BottomSheetType: <BottomSheetPriceRange />,
+    },
+  ];
+
+  return (
+    <div className='border-black-626/15 sticky top-[50px] z-50 flex gap-2 border-b bg-white px-4 py-3 shadow-[0_1px_5px_rgba(0,0,0,0.15)]'>
+      {FILTERS.map(({ key, label, defaultValue, valueSize = 18, BottomSheetType }) => (
+        <div
+          key={key}
+          className='border-black-626/15 flex w-full flex-col justify-between gap-3 rounded-[15px] border bg-white p-2'
+        >
+          <Txt align='center' className='text-gray-070'>
+            {label}
+          </Txt>
+          <Txt
+            align='center'
+            size={valueSize}
+            className={valueSize === 16 ? 'leading-4 whitespace-pre-line' : ''}
+          >
+            {defaultValue}
+          </Txt>
+          {BottomSheetType}
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/components/domain/realEstates/index.ts
+++ src/components/domain/realEstates/index.ts
@@ -1,3 +1,6 @@
-export { default as EstateDescription } from './EstateDescription';
 export { default as BottomButton } from './BottomButton';
+export { default as BottomSheetDealType } from './BottomSheetDealType';
+export { default as BottomSheetPriceRange } from './BottomSheetPriceRange';
+export { default as EstateDescription } from './EstateDescription';
 export { default as EstateHeader } from './EstateHeader';
+export { default as FilterEstatesOptions } from './FilterEstatesOptions';
