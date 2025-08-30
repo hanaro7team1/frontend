0cb83b074c7addc208d5d46c3fb5be72e3702838
ryd10227 <ryd10227@gmail.com>
2025-08-30 23:16:26 +0900

    Feat: 매물 목록 화면

--- src/app/real-estates/page.tsx
+++ src/app/real-estates/page.tsx
@@ -1,10 +1,21 @@
 import { BottomTabNav, Header } from '@/components/common';
+import { FilterEstatesOptions } from '@/components/domain/realEstates';
+import { RoomList } from '@/components/domain/stays';
 
-export default function RealEstatesPage() {
+type Props = {
+  searchParams: Promise<RealEstatesSearchParams>;
+};
+
+export default async function RealEstatesPage({ searchParams }: Props) {
+  const searchParam = await searchParams;
+  const isAdmin = false; // TODO: 추후에 세션에서 관리자 여부 읽어오기
+
   return (
     <div>
-      <Header title='매물 찾기' />
-      <BottomTabNav />
+      <Header title='매물 찾기' bgColor='green' />
+      <FilterEstatesOptions />
+      <RoomList searchParams={searchParam} />
+      {!isAdmin && <BottomTabNav />}
     </div>
   );
 }
