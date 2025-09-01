ff64a8318277568abfa8e48f9669f0897147de89
ahhyun <ahhyun1106@gmail.com>
2025-09-01 17:21:31 +0900

    Refactor: 전화번호 auto hyphen util로 분리

--- src/app/admin/mypage/contact/page.tsx
+++ src/app/admin/mypage/contact/page.tsx
@@ -1,16 +1,18 @@
-'use client'
-import { Button, Input, Txt } from "@/components/atoms";
-import { Header } from "@/components/common";
-import { useState } from "react";
-import Image from "next/image";
-import NoticeModal from "@/components/domain/admin/mypage/NoticeModal";
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import { Button, Input, Txt } from '@/components/atoms';
+import { Header } from '@/components/common';
+import NoticeModal from '@/components/domain/admin/mypage/NoticeModal';
+import { phoneHyphen } from '@/utils/common/phoneHyphen';
 
 export default function AdminContactPage() {
   // db
-  const hostPhone = "010-1234-1234";
+  const hostPhone = '010-1234-1234';
 
   const [phone, setPhone] = useState(hostPhone);
-  const [newPhone, setNewPhone] = useState("");
+  const [newPhone, setNewPhone] = useState('');
   const [openNotice, setOpenNotice] = useState(false);
 
   const editPhone = () => {
@@ -18,38 +20,34 @@ export default function AdminContactPage() {
     setOpenNotice(true);
   };
 
-  const phoneHyphen = (h: string) => {
-    const digits = h.replace(/\D/g, '');
-
-    if (digits.length < 4) return digits;
-    if (digits.length < 8) return `${digits.slice(0, 3)}-${digits.slice(3)}`;
-    if (digits.length == 10) return `${digits.slice(0,3)}-${digits.slice(3,6)}-${digits.slice(6,10)}`;
-
-    return `${digits.slice(0, 3)}-${digits.slice(3, 7)}-${digits.slice(7, 11)}`;
-  };
-
   return (
     <>
-      <Header title="전화번호 변경"/>
-      
-      <div className="p-8 flex flex-col gap-4">
-        <Txt size={24} className="text-gray-070">기존 전화번호</Txt>
-        <div className="flex items-center gap-3">
-          <Image src='/icons/Ic_Phone_big.svg' alt="수화기" width={35} height={35}/>
+      <Header title='전화번호 변경' />
+
+      <div className='flex flex-col gap-4 p-8'>
+        <Txt size={24} className='text-gray-070'>
+          기존 전화번호
+        </Txt>
+        <div className='flex items-center gap-3'>
+          <Image src='/icons/Ic_Phone_big.svg' alt='수화기' width={35} height={35} />
           <Txt size={24}>{phone}</Txt>
         </div>
-      
-        <div className="flex flex-col gap-4 mt-[30px]">
-          <Txt size={24} className="text-gray-070">변경할 전화번호</Txt>
-          <Input value={phoneHyphen(newPhone)} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewPhone(e.target.value)}
-            placeholder="변경할 전화번호를 입력하세요"/>
+
+        <div className='mt-[30px] flex flex-col gap-4'>
+          <Txt size={24} className='text-gray-070'>
+            변경할 전화번호
+          </Txt>
+          <Input
+            value={phoneHyphen(newPhone)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewPhone(e.target.value)}
+            placeholder='변경할 전화번호를 입력하세요'
+          />
         </div>
-        
-        <Button title="변경하기" color="pink" onClick={editPhone}/>
+
+        <Button title='변경하기' color='pink' onClick={editPhone} />
       </div>
 
-      <NoticeModal open={openNotice} text="전화번호" />
+      <NoticeModal open={openNotice} text='전화번호' />
     </>
   );
 }
-
--- src/components/domain/admin/add/steps/step4/Step4.tsx
+++ src/components/domain/admin/add/steps/step4/Step4.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { Input, Txt } from '@/components/atoms';
+import { phoneHyphen } from '@/utils/common/phoneHyphen';
 
 export default function AddOwner() {
+  const [newPhone, setNewPhone] = useState('');
+
   return (
     <>
       <Txt>사랑방 주인의 성함은 무엇인가요?</Txt>
@@ -11,7 +15,11 @@ export default function AddOwner() {
         </Txt>
       </div>
       <Txt>사랑방 주인의 전화번호를 입력하세요</Txt>
-      <Input placeholder='예시) 01012345567' />
+      <Input
+        placeholder='예시) 01012345567'
+        value={phoneHyphen(newPhone)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewPhone(e.target.value)}
+      />
     </>
   );
 }
