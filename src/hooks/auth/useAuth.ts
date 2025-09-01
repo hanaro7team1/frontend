f2fb54437d05c4419bd0aec5452b07eaca4c1f46
ryd10227 <ryd10227@gmail.com>
2025-09-01 16:16:10 +0900

    Refactor: 인증 필요하지 않은 axios instance - 모든 컴포넌트용 파일 분리

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { authApi } from '@/lib/axios';
+import { authApi } from '@/lib/axios-client';
 
 interface User {
   loginId: string;
