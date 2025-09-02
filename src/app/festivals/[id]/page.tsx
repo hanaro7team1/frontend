3691739a9ed3341d4c9c58caa0625b6d86802935
Kunbam7 <kunh9905@gmail.com>
2025-09-02 14:11:57 +0900

    Feat: build d-day cal util & Date format

--- src/app/festivals/[id]/page.tsx
+++ src/app/festivals/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Txt } from "@/components/atoms";
 import { BottomTabNav, Header } from "@/components/common";
 import Image from "next/image";
 import { CalendarCheck, HandCoinsIcon, Link, MapPinned } from 'lucide-react';
-import { formatDate } from "@/utils/stays/stays";
+import { dDayCal } from "@/utils/festivals/dateCal";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -15,18 +15,23 @@ export default async function FestivalDetailPage({ params }: Props) {
   const smapleData = {
     dataId: 1,
     title: "신정호 별빛축제",
-    startDateS: "2025-10-10",
-    endDateS: "2025-10-20",
+    startDate: "2025-09-10",
+    endDate: "2025-10-20",
     describtion: "<신정호 별빛축제>는 한여름 밤의 더위를 피해 신정호에서 펼쳐지는 축제이다. 1998년 별빛 특선 영화제, 신정호 토요문화 한마당에서 시작 되었으며 2007년 한여름밤의 신정호 별빛축제로 명칭이 변경되어 개최해오고 있다. 지역 예술인 공연, 영화 상영, 별자리 관측 등이 진행된다.",
     addr: "충청남도 아산시 신정로 616 (방축동)",
     price: 0,
     url: "주소"
   }
-  const {dataId, title, startDateS, endDateS, describtion, addr, price, url} = smapleData;
-  const toKstDate = (ymd: string) => new Date(`${ymd}T00:00:00+09:00`);
-  const startDate = toKstDate(startDateS);
-  const endDate = toKstDate(endDateS);
+  const {dataId, title, startDate, endDate, describtion, addr, price, url} = smapleData;
   
+  const dateFormat = (s: string) => `${s.slice(0,4)}.${s.slice(5,7)}.${s.slice(8,10)}`;
+
+  const dDay = (d: string) => {
+    const n = dDayCal(d);
+    if(!Number.isFinite(n)) return '';
+    
+    return n > 0 ? `D-${n}` : n === 0 ? 'D-Day' : `D+${Math.abs(n)}`;
+};
 
   return <div>
     <Header title="축제 자세히 보기" bgColor="white" />
@@ -37,13 +42,13 @@ export default async function FestivalDetailPage({ params }: Props) {
         <div className="flex items-center justify-between">
           <Txt size={24} weight="bold">{title}</Txt>
           <div className="inline-flex items-center w-fit rounded-[8px] bg-gray-6d6 px-3">
-            <Txt size={22} className="text-green-49d ">D-7</Txt>
+            <Txt size={22} className="text-green-49d ">{dDay(startDate)}</Txt>
           </div>
         </div>
 
         <div className={`${commonPosision}`}>
           <CalendarCheck color="var(--code-theme1)" size={25}/>
-          <Txt size={24}>{formatDate(startDate)} ~ {formatDate(endDate)}</Txt>
+          <Txt size={24}>{dateFormat(startDate)} ~ {dateFormat(endDate)}</Txt>
         </div>
 
         <div>
