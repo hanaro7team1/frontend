8b57f153f8da2b2e73e5a9efbf4df3e298fc0e23
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-02 11:11:25 +0900

    Design: 비밀번호 변경 페이지 UI 수정

--- src/app/admin/mypage/pwd/page.tsx
+++ src/app/admin/mypage/pwd/page.tsx
@@ -1,17 +1,18 @@
-'use client'
-import { Button, Input, Txt } from "@/components/atoms";
-import { Header, Modal } from "@/components/common";
-import { PasswordField } from "@/components/domain/admin/auth/PasswordField";
-import NoticeModal from "@/components/domain/admin/mypage/NoticeModal";
-import useValidation from "@/hooks/auth/useValidation";
-import { useState } from "react";
+'use client';
+
+import { useState } from 'react';
+import { Button, Input, Txt } from '@/components/atoms';
+import { Header, Modal } from '@/components/common';
+import { PasswordField } from '@/components/domain/admin/auth/PasswordField';
+import NoticeModal from '@/components/domain/admin/mypage/NoticeModal';
+import useValidation from '@/hooks/auth/useValidation';
 
 export default function AdminPwdPage() {
-  const txtPosition = "flex flex-col gap-2";
+  const txtPosition = 'flex flex-col gap-2 ';
 
   const [password, setPassword] = useState('');
 
-  const {form, errors, handleChange} = useValidation();
+  const { form, errors, handleChange } = useValidation();
 
   const [openNotice, setOpenNotice] = useState(false);
   const editPwd = () => {
@@ -20,9 +21,9 @@ export default function AdminPwdPage() {
 
   return (
     <form>
-      <Header className="mb-[50px]" title="비밀번호 변경" />
-    
-      <div className="flex flex-col gap-9 p-8">
+      <Header className='mb-13' title='비밀번호 변경' />
+
+      <div className='flex flex-col gap-9 p-8'>
         <div className={`${txtPosition}`}>
           <Txt size={24}>기존 비밀번호</Txt>
           <PasswordField
@@ -33,37 +34,36 @@ export default function AdminPwdPage() {
         </div>
 
         <div className={`${txtPosition}`}>
-          <Txt size={24}>비밀번호</Txt>
+          <Txt size={24}>변경할 비밀번호</Txt>
           <PasswordField
             value={form.password}
             placeholder={'영문자,숫자,특수문자를 포함한 8~20자'}
             onChange={(v) => handleChange('password', v)}
           />
           {errors.password && (
-            <Txt size={18} className='text-pink-a76'>
+            <Txt size={18} className='text-pink-a76 ml-4' weight='regular'>
               {errors.password}
             </Txt>
           )}
         </div>
         <div className={`${txtPosition}`}>
-          <Txt size={24}>비밀번호 확인</Txt>
+          <Txt size={24}>변경할 비밀번호 확인</Txt>
           <PasswordField
             value={form.confirmPassword}
             placeholder={'변경할 비밀번호를 한번 더 입력하세요'}
             onChange={(v) => handleChange('confirmPassword', v)}
           />
           {errors.confirmPassword && (
-            <Txt size={18} className='text-pink-a76'>
+            <Txt size={18} className='text-pink-a76 ml-4' weight='regular'>
               {errors.confirmPassword}
             </Txt>
           )}
         </div>
 
-        <Button title="변경하기" color="pink" onClick={editPwd}/>
-      </div> 
+        <Button title='변경하기' color='pink' onClick={editPwd} />
+      </div>
 
-      <NoticeModal open={openNotice} text="비밀번호"/>
+      <NoticeModal open={openNotice} text='비밀번호' />
     </form>
   );
 }
-
