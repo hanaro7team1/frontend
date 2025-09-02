0746dff5918e0bcded1dd0f52da59d4d19e5733d
Kunbam7 <kunh9905@gmail.com>
2025-09-02 11:04:49 +0900

    Design: change listBox's txt & page ad txt posision

--- src/app/festivals/page.tsx
+++ src/app/festivals/page.tsx
@@ -55,11 +55,11 @@ export default function FestivalsPage() {
     <Header title='지역 축제' bgColor='green'/>
 
     <div className='p-4'>
-      <div className='relative rounded-[10px] flex justify-end 
+      <div className='relative rounded-[10px] flex justify-center 
                     border border-green-edc bg-green-2f1
                     items-center'>  
           <Image src="/images/Img_Festival.svg" alt='쇼핑' width={107} height={107} className='absolute left-0' />
-          <Txt size={24} weight='medium' className='text-center mr-15'>즐거운 지역축제<br />함께 시도해요!</Txt>
+          <Txt size={24} weight='medium' className='text-center ml-15'>즐거운 지역축제<br />함께 시도해요!</Txt>
       </div>
     </div>
 
