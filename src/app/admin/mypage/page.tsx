103def1f5880b1dffd258e05d003632a05c4a7f1
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-02 10:55:06 +0900

    Design: 마이페이지 UI 수정

--- src/app/admin/mypage/page.tsx
+++ src/app/admin/mypage/page.tsx
@@ -1,39 +1,71 @@
-'use client'
-import { useRouter} from 'next/navigation';
-import { Txt } from "@/components/atoms";
-import { Header } from "@/components/common";
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Txt } from '@/components/atoms';
+import { Header, Modal } from '@/components/common';
 import ChangeBox from '@/components/domain/admin/mypage/ChangeBox';
 
 export default function AdminMyPage() {
   const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const hostMember = {
-    villageName: "가람마을",
-    phone: "010-1234-1234",
-  }
-  const {villageName, phone} = hostMember
+    villageName: '00마을',
+    phone: '010-1234-5678',
+  };
+  const { villageName, phone } = hostMember;
 
   return (
     <>
       <Header title="내 정보" />
-      <div className='bg-gray-484/10 p-11 flex gap-7 items-center justify-center'>
-          <div className="w-[70px] h-[70px] rounded-full bg-[#00A49D]  grid place-items-center">
-            <Image src='/images/Img_Mypage_Profile.svg' alt='프로파일' width={50} height={50} className="object-contain"/>
-          </div>
+
+      <div className="bg-gray-484/10 border-b-black-626/15 flex items-center justify-center gap-7 border p-8">
+        <div className="bg-green-49d relative grid h-[70px] w-[70px] place-items-center rounded-full">
+          <Image
+            src="/images/Img_Mypage_Profile.svg"
+            alt="프로필"
+            width={62}
+            height={62}
+            className="object-contain pb-3 pl-1"
+          />
+        </div>
+        <section className="flex flex-col items-start">
           <Txt size={30}>{villageName} 관리자</Txt>
+          <button
+            type="button"
+            onClick={() => setIsModalOpen(true)}
+            className="text-gray-500 underline underline-offset-2"
+          >
+            로그아웃
+          </button>
+        </section>
       </div>
 
-      <div className='flex flex-col p-8 gap-9'>
-          <ChangeBox buttonTxt={'전화번호 변경'} phoneNum={phone} />
-          <ChangeBox buttonTxt={'비밀번호 변경'} />
+      <div className="mt-9 flex flex-col gap-9 px-6">
+        <ChangeBox buttonTxt="전화번호 변경" phoneNum={phone} />
+        <ChangeBox buttonTxt="비밀번호 변경" />
       </div>
 
-        <button type="button" onClick={() => router.push('/admin/mypage/quit')}
-          className="mt-[150px] block w-fit mx-auto bg-transparent
-                      underline underline-offset-4 decoration-gray-070">
-          <Txt className='text-gray-070'>탈퇴하기</Txt>
-        </button>
+      <Link
+        href="/admin/mypage/quit"
+        className="fixed bottom-12 left-1/2 -translate-x-1/2 text-gray-500 underline underline-offset-2"
+      >
+        탈퇴하기
+      </Link>
+
+      {isModalOpen && (
+        <Modal
+          grayBtnText="아니요"
+          greenBtnText="네"
+          onClickGrayBtn={() => setIsModalOpen(false)}
+          onClickGreenBtn={() => router.push('/auth')}
+        >
+          로그아웃 하시겠어요?
+        </Modal>
+      )}
     </>
   );
 }
-
