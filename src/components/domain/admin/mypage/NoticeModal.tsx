193b1b8b08187c4f641ed8467296bc52d307d2de
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-02 13:23:51 +0900

    Refactor: 비밀번호 변경 후 내 정보 페이지로 이동

--- src/components/domain/admin/mypage/NoticeModal.tsx
+++ src/components/domain/admin/mypage/NoticeModal.tsx
@@ -1,28 +1,33 @@
-'use client'
+'use client';
 
-import { Button, Txt } from "@/components/atoms";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
+import { Button, Txt } from '@/components/atoms';
 
 type Props = {
-    open: boolean;
-    text?: string;
+  open: boolean;
+  text?: string;
 };
 
-export default function NoticeModal({open, text}: Props) {
-    const router = useRouter();
-    if(!open){ return null;}
-    
-    const message = `${text}가 변경되었어요`;
+export default function NoticeModal({ open, text }: Props) {
+  const router = useRouter();
+  if (!open) {
+    return null;
+  }
 
-    return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4">
-            <div className="w-full mx-4 h-[193px] shrink-0 rounded-[10px]
-                    border border-[#DEDEDE] bg-white p-5 shadow-md
-                    flex flex-col items-center justify-between">
-                <Txt size={26} className="text-center">{message}</Txt>
-                <Button title="확인" color="pink" className="w-[337px] h-[56px]"
-                    onClick={() => router.back()}/>
-            </div>
-        </div>
-    );
+  const message = `${text}가 변경되었어요`;
+
+  return (
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4'>
+      <div className='mx-4 flex h-[193px] w-full shrink-0 flex-col items-center justify-center gap-9 rounded-[10px] border border-[#DEDEDE] bg-white p-5 shadow-md'>
+        <Txt size={26} className='text-center'>
+          {message}
+        </Txt>
+        <Button
+          title='확인'
+          color='pink'
+          onClick={() => router.push('/admin/mypage')}
+        />
+      </div>
+    </div>
+  );
 }
