f01df9e35a12fc5337c39ae2f232627387d1bf7e
Kunbam7 <kunh9905@gmail.com>
2025-09-03 01:35:54 +0900

    Refactor: move to festival's detail inf when press button

--- src/components/domain/festivals/ListBox.tsx
+++ src/components/domain/festivals/ListBox.tsx
@@ -5,15 +5,15 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 type Props = {
+    id: number;
     img: string;
     title: string;
     startDate: string;
     endDate: string;
     city: string;
-    url: string;
 }
 
-export default function ListBox({img, title, startDate, endDate, city, url}:Props) {
+export default function ListBox({id, img, title, startDate, endDate, city}:Props) {
     const router = useRouter();
 
     return <>
@@ -25,7 +25,7 @@ export default function ListBox({img, title, startDate, endDate, city, url}:Prop
                 <Txt>{title}</Txt>
                 <Txt size={15}>{city}</Txt>
                 <Txt size={18}>{startDate} ~ {endDate}</Txt>
-                <Button title="상세보기" color="gray" className="h-[35px] mt-3" onClick={() => router.push(`${url}`)}/>
+                <Button title="상세보기" color="gray" className="h-[35px] mt-3" onClick={() => router.push(`/festivals/${id}`)}/>
             </div>
         </div>
     </>;
