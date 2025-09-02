7ccf71f8ebd40d5a2ff0037085a497bae40d2c47
ryd10227 <ryd10227@gmail.com>
2025-09-02 12:54:20 +0900

    Refactor: 관리자 여부 가져오는 util함수 (클라이언트 컴포넌트용 ) 예외처리

--- src/utils/auth/auth-client.ts
+++ src/utils/auth/auth-client.ts
@@ -5,6 +5,8 @@
  * @returns {boolean} isAdmin
  */
 export function getIsAdminFromLocalStorage() {
+  if (typeof window === 'undefined') return false; // 서버 컴포넌트에서의 사용 방지
+
   const role = localStorage.getItem('role');
   const isAdmin = role === 'ROLE_ADMIN';
   return isAdmin;
