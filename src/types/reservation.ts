f07dc6d100bcad3edc3912f2fec3ba0a5cf2e2cb
ryd10227 <ryd10227@gmail.com>
2025-09-04 04:20:04 +0900

    Chore: 안 쓰는 dummy data 삭제

--- src/types/reservation.ts
+++ src/types/reservation.ts
@@ -1,5 +1,13 @@
 import { ReservationStatus } from '@/enums/reservation';
 
+export type ReservationInfo = {
+  id: number;
+  name: string;
+  location: string;
+  status: '방문 중' | '방문 전' | '방문 완료' | '예약 취소';
+  imgUrl: string;
+};
+
 export type ReservationPayload = {
   id: number;
   confirmedDate: string;
