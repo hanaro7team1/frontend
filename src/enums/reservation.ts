0c4486e7ef8fd1375f02580ab6f9d7f53dc1eab2
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-03 17:46:14 +0900

    Feat: 사랑방 방문 상태 캡슐 Enum 구현

--- src/enums/reservation.ts
+++ src/enums/reservation.ts
@@ -3,3 +3,10 @@ export enum ReservationStatus {
   RESERVED = 'RESERVED', // 예약 확정
   CANCELLED = 'CANCELLED', // 예약 취소
 }
+
+export enum ReservationViewStatus {
+  UPCOMING = '방문 전',
+  IN_PROGRESS = '방문 중',
+  COMPLETED = '방문 완료',
+  CANCELLED = '예약 취소',
+}
