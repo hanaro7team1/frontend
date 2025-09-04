8000a252db5e5a87eb5ce45821598f48f7e66e6b
ahhyun <ahhyun1106@gmail.com>
2025-09-04 22:35:30 +0900

    Feat: 사랑방 정보 수정하기 상세 주소 추가

--- src/app/admin/stays/[id]/page.tsx
+++ src/app/admin/stays/[id]/page.tsx
@@ -13,14 +13,26 @@ export default async function AdminStayEditPage({ params }: Props) {
   const { id } = await params;
 
   const { data } = await publicApi.get<StayDetailResponseType>(`/api/stays/${id}`);
-  const { title, address, capacity, areaSize, description, images, id: stayId } = data;
+  const {
+    title,
+    address,
+    detailAddress,
+    capacity,
+    areaSize,
+    description,
+    images,
+    id: stayId,
+  } = data;
 
   return (
     <div className='flex flex-col gap-4'>
       <Header title='사랑방 정보 수정하기' />
 
       <main className='flex flex-col gap-10 px-5'>
-        <StayInfoCard data={{ imageUrl: images[0], stayId, title, address }} isAdmin />
+        <StayInfoCard
+          data={{ imageUrl: images[0], stayId, title, address, detailAddress }}
+          isAdmin
+        />
         <EditStay data={{ areaSize, capacity, description, stayId }} />
       </main>
     </div>
