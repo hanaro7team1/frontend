012ae695562df13c053d85deca412cb561d0eae7
Kunbam7 <kunh9905@gmail.com>
2025-09-05 10:52:02 +0900

    Fix: reservation & filter bug fix

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -1,9 +1,9 @@
-import { Header } from "@/components/common";
-import ReservationCard from "@/components/domain/reservations/ReservationCard";
-import { getIsAdmin } from "@/utils/auth/auth-server";
-import FilterReserv from "@/components/domain/reservations/FilterReserv";
-import { publicApi } from "@/lib/axios";
-import { ReservationsResponse } from "@/types/reservation";
+import { serverPrivateApi } from '@/lib/axios-server';
+import { Header } from '@/components/common';
+import FilterReserv from '@/components/domain/reservations/FilterReserv';
+import ReservationCard from '@/components/domain/reservations/ReservationCard';
+import { getIsAdmin } from '@/utils/auth/auth-server';
+import { ReservationsResponse } from '@/types/reservation';
 
 export type ReservationStatus = '예약됨' | '방문 완료' | '취소됨';
 export type ReservationsSearchParams = {
@@ -17,16 +17,21 @@ type Props = {
 export default async function ReservationsPage({ searchParams }: Props) {
   const searchParam = await searchParams;
   const isAdmin = await getIsAdmin();
-  
-  const { data } = await publicApi.get<ReservationsResponse>('/api/reservations', { params: searchParam });
 
-  return <>
-    <Header title={isAdmin? "우리 마을 사랑방 예약 목록" : "나의 예약 목록"} bgColor="white" withoutBorder/>
-    <FilterReserv searchParams={searchParam} isAdmin={isAdmin}/>
-    <div className="flex flex-col p-3 gap-3">
-      {data.dtoList.map((reservation) => (
-        <ReservationCard key={reservation.id} data={reservation} />
-      ))}
-    </div>
-  </>;
+  const api = await serverPrivateApi();
+  const { data } = await api.get<ReservationsResponse>('/api/reservations', {
+    params: searchParam,
+  });
+
+  return (
+    <>
+      <Header title={isAdmin ? '우리 마을 사랑방 예약 목록' : '나의 예약 목록'} withoutBorder />
+      <FilterReserv searchParams={searchParam} isAdmin={isAdmin} />
+      <div className='flex flex-col gap-3 p-3'>
+        {data.dtoList.map((reservation) => (
+          <ReservationCard key={reservation.id} data={reservation} />
+        ))}
+      </div>
+    </>
+  );
 }
