c9c3843c9bf22af884ee7d9eae793ddf5f67fb5e
ryd10227 <ryd10227@gmail.com>
2025-09-05 15:12:27 +0900

    Feat: 사랑방 예약 가능 날짜 관리 모달 구현

--- src/types/admin.ts
+++ src/types/admin.ts
@@ -4,3 +4,13 @@ export type AdminReservationResponse = {
   inProgressCnt: number;
   completedCnt: number;
 };
+
+export type StayAvailableDatesResponse = {
+  yearMonth: string; // "2025-09"
+  openDates: string[]; // [ "2025-09" ]
+  reservedDates: string[]; // [ "2025-09" ]
+  hasOpenPrev: boolean;
+  hasOpenNext: boolean;
+  hasReservedPrev: boolean;
+  hasReservedNext: boolean;
+};
