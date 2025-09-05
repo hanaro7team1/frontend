7829a28de45b7a7ec5897ae79872a59d5fbf0ad0
ahhyun <ahhyun1106@gmail.com>
2025-09-05 23:41:52 +0900

    Feat: 하나원큐 스플래시 페이지

--- src/components/domain/realEstates/BottomButton.tsx
+++ src/components/domain/realEstates/BottomButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { Button } from '@/components/atoms';
 
 type Props = {
@@ -10,12 +11,9 @@ type Props = {
 export default function BottomButton({ text = '문의하기', disabled = false }: Props) {
   return (
     <div className='fixed inset-x-0 bottom-0 z-50 mx-auto w-full border-t border-gray-200 bg-white p-4 sm:max-w-sm'>
-      <Button
-        title={text}
-        color='green'
-        disabled={disabled}
-        onClick={() => alert('문의하기 클릭')}
-      />
+      <Link href='/hana' className='block'>
+        <Button title={text} color='green' disabled={disabled} />
+      </Link>
     </div>
   );
 }
