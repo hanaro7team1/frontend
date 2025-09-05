c0b8b680dd7b6a8dd5293b95554b6f6659909b54
ryd10227 <ryd10227@gmail.com>
2025-09-05 11:33:13 +0900

    Design: 전체 레이아웃 - 하단 여백 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body className='flex h-screen items-center justify-center overflow-hidden antialiased'>
-        <main className='h-full w-full overflow-y-auto bg-white pb-25 shadow-[0_0_10px_rgba(0,0,0,0.3)] sm:max-w-sm [&::-webkit-scrollbar]:hidden'>
+        <main className='h-full w-full overflow-y-auto bg-white shadow-[0_0_10px_rgba(0,0,0,0.3)] sm:max-w-sm [&::-webkit-scrollbar]:hidden'>
           {children}
         </main>
       </body>
