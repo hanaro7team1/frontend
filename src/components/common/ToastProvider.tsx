9534c932c856e57701e7b145d9fbd03633f799b8
ryd10227 <ryd10227@gmail.com>
2025-09-06 17:11:38 +0900

    Fix: 토스트 배경 불투명하게 변경 가시성 증가

--- src/components/common/ToastProvider.tsx
+++ src/components/common/ToastProvider.tsx
@@ -68,7 +68,7 @@ export function ToastProvider({ children }: PropsWithChildren<Props>) {
           )}
         >
           {/* Toast UI */}
-          <div className='bg-gray-070/70 flex w-full items-center gap-4 rounded-lg px-4 py-2 shadow-[0px_0px_5px_0px_rgba(0,0,0,0.15)] backdrop-blur-md'>
+          <div className='bg-gray-070 flex w-full items-center gap-4 rounded-lg px-4 py-2 shadow-[0px_0px_5px_0px_rgba(0,0,0,0.15)] backdrop-blur-md'>
             <Image
               src={iconSrcMap[toastType]}
               alt='check'
