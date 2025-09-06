abef725d2d94fd8ea576a1ccfb7e87213326621b
ryd10227 <ryd10227@gmail.com>
2025-09-07 00:54:55 +0900

    Design: 권한 없음 페이지 별돌이 이미지 수정

--- src/app/invalidAccess/page.tsx
+++ src/app/invalidAccess/page.tsx
@@ -5,7 +5,7 @@ import { GoBackBtn } from '@/components/common';
 export default function InvalidAccessPage() {
   return (
     <div className='flex h-screen flex-col items-center justify-center gap-4 px-6 text-center'>
-      <Image src='/images/Img_Home_City.svg' alt='별돌이 이미지' width={100} height={100} />
+      <Image src='/images/Img_Byeoldol-Sosad.svg' alt='별돌이 이미지' width={200} height={200} />
       <Txt size={60} weight='bold' className='text-green-49d'>
         403
       </Txt>
@@ -13,7 +13,7 @@ export default function InvalidAccessPage() {
         앗! 접근 권한이 없어요
       </Txt>
 
-      <Txt weight='medium' className='text-green-49d my-10'>
+      <Txt weight='medium' className='my-5'>
         요기있는 별돌이가 다시 데려다줄게요!
       </Txt>
 
