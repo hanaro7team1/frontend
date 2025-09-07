9351724f31c0b30945d9234d814a168ab0357236
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-07 23:24:34 +0900

    Refactor: useRouter 대신 Link 활용해 서버 컴포넌트로 구현

--- src/components/domain/admin/AddStayButton.tsx
+++ src/components/domain/admin/AddStayButton.tsx
@@ -1,23 +1,19 @@
-'use client';
-
 import { Plus } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { Txt } from '@/components/atoms';
 
 export default function AddStayButton() {
-  const router = useRouter();
-
   return (
     <section className='px-4 pt-3'>
-      <button
-        className={'bg-pink-a76/10 flex w-full items-center gap-3 rounded-[10px] py-[21px] pl-5'}
-        onClick={() => router.push('/admin/stays/add')}
+      <Link
+        href='/admin/stays/add'
+        className='bg-pink-a76/10 flex w-full items-center gap-3 rounded-[10px] py-[21px] pl-5'
       >
-        <Plus className='text-pink-a76 items-center' size={22} />
+        <Plus className='text-pink-a76' size={22} />
         <Txt className='text-pink-a76' size={22} weight='bold'>
           사랑방 등록하기
         </Txt>
-      </button>
+      </Link>
     </section>
   );
 }
--- src/components/domain/admin/MenuTabs.tsx
+++ src/components/domain/admin/MenuTabs.tsx
@@ -1,32 +1,25 @@
-'use client';
-
 import { House, List, User } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { Txt } from '@/components/atoms';
 
 export default function MenuTabs() {
-  const router = useRouter();
-
   const commonClasses = 'flex flex-col justify-between rounded-[10px] p-6 text-left';
 
   return (
     <div className='flex flex-col gap-3'>
-      <button
+      <Link
+        href='/admin/stays'
         className='bg-pink-a76/70 flex h-24 w-full items-center justify-between rounded-[10px] p-6 text-white'
-        onClick={() => router.push('/admin/stays')}
       >
         <Txt className='text-white' weight='bold'>
           우리 마을 사랑방
           <br /> 관리하기
         </Txt>
         <House size={28} className='text-white' />
-      </button>
+      </Link>
 
       <div className='grid grid-cols-2 gap-3'>
-        <button
-          className={`${commonClasses} bg-pink-a76/10 h-36`}
-          onClick={() => router.push('/stays')}
-        >
+        <Link href='/stays' className={`${commonClasses} bg-pink-a76/10 h-36`}>
           <Txt className='text-pink-a76' weight='bold'>
             전체 사랑방 <br />
             구경하기
@@ -34,12 +27,9 @@ export default function MenuTabs() {
           <div className='flex items-end justify-end'>
             <List size={28} className='text-pink-a76' />
           </div>
-        </button>
+        </Link>
 
-        <button
-          className={`${commonClasses} bg-pink-a76/50 h-36 text-white`}
-          onClick={() => router.push('/admin/mypage')}
-        >
+        <Link href='/admin/mypage' className={`${commonClasses} bg-pink-a76/50 h-36 text-white`}>
           <Txt className='text-white' weight='bold'>
             내 정보 <br />
             수정하기
@@ -47,7 +37,7 @@ export default function MenuTabs() {
           <div className='flex items-end justify-end'>
             <User size={28} className='text-white' />
           </div>
-        </button>
+        </Link>
       </div>
     </div>
   );
