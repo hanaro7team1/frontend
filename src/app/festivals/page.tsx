2436c2c4bcff9a0b1ddd27f2e98b31b681f34101
ryd10227 <ryd10227@gmail.com>
2025-09-08 23:01:48 +0900

    Fix: 배포 에러 7 /festivals

--- src/app/festivals/page.tsx
+++ src/app/festivals/page.tsx
@@ -1,14 +1,15 @@
-import Image from 'next/image';
-import { Txt } from '@/components/atoms';
-import { BottomTabNav, Header } from '@/components/common';
-import FestivalList from '@/components/domain/festivals/FestivalList';
+// import Image from 'next/image';
+// import { Txt } from '@/components/atoms';
+// import { BottomTabNav, Header } from '@/components/common';
+// import FestivalList from '@/components/domain/festivals/FestivalList';
+import { Header } from '@/components/common';
 
 export default async function FestivalsPage() {
   return (
     <>
       <Header title='지역 축제' bgColor='green' />
 
-      <div className='p-4'>
+      {/* <div className='p-4'>
         <div className='border-green-edc bg-green-2f1 relative flex items-center justify-center rounded-[10px] border'>
           <Image
             src='/images/Img_Festival.svg'
@@ -27,7 +28,7 @@ export default async function FestivalsPage() {
 
       <FestivalList />
 
-      <BottomTabNav />
+      <BottomTabNav /> */}
     </>
   );
 }
