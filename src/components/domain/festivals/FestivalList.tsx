d4b24ad15782e82be10878f2cf5291b582d13bae
Kunbam7 <kunh9905@gmail.com>
2025-09-08 17:23:14 +0900

    Refactor: scroll instance save

--- src/components/domain/festivals/FestivalList.tsx
+++ src/components/domain/festivals/FestivalList.tsx
@@ -1,18 +1,24 @@
 'use client'
 
 import { usePublicData } from "@/hooks/api/useApi";
+import useSWRInfinite from 'swr/infinite'
 import ListBox from "./ListBox";
-import { FestivalListResponse } from "@/types/festivals";
+import { FestivalListItemResponse, FestivalListResponse } from "@/types/festivals";
+import { useEffect, useMemo, useRef } from "react";
 
 type Props = {
     firstList: FestivalListResponse;
 };
+const getKey = (pageIndex, prevPageData) => {
+    
+}
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function FestivalList({ firstList }: Props) {
-    const { data } = usePublicData<FestivalListResponse>('/api/festivals');
+    // const { data } = usePublicData<FestivalListResponse>('/api/festivals');
 
+    const { data } = useSWRInfinite( () => `/api/festivals/&page=${1 + page} `, fetcher)
     
-
     return <>
         <div className='flex flex-col gap-8 p-5 pb-25'>
             {data?.dtoList.map((festival) => (
