c6031c318971f8ea8f296b3da2fae84d38dd97e5
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-09-08 10:00:19 +0900

    Init: frontend CICD

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sido-upload.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
