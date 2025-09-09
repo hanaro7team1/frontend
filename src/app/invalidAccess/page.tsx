ae1dd26506da8b0d075206c5dce7bd55b209e641
ryd10227 <ryd10227@gmail.com>
2025-09-09 10:41:09 +0900

    Design: 리스트 비었을 때 이미지 허수아비로

--- src/app/invalidAccess/page.tsx
+++ src/app/invalidAccess/page.tsx
@@ -4,8 +4,8 @@ import { GoBackBtn } from '@/components/common';
 
 export default function InvalidAccessPage() {
   return (
-    <div className='flex h-screen flex-col items-center justify-center gap-4 px-6 text-center'>
-      <Image src='/images/Img_Byeoldol-Sosad.svg' alt='별돌이 이미지' width={200} height={200} />
+    <div className='flex h-screen flex-col items-center justify-center gap-1 px-6 text-center'>
+      <Image src='/images/Img_TUNG.png' alt='허수아비가 된 별돌이' width={200} height={200} />
       <Txt size={60} weight='bold' className='text-green-49d'>
         403
       </Txt>
@@ -13,7 +13,7 @@ export default function InvalidAccessPage() {
         앗! 접근 권한이 없어요
       </Txt>
 
-      <Txt weight='medium' className='my-5'>
+      <Txt weight='medium' className='my-4'>
         요기있는 별돌이가 다시 데려다줄게요!
       </Txt>
 
